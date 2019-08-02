3dfe8549e1968eb5adec03a499f06d864af5c4ca
LoveHQH <huy441998@gmail.com>
2019-08-02 10:44:32 +0700

    commit formSuaTT

---
+++ ThucTapNhom2019-Project1/ttNhanVien.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace ThucTapNhom2019_Project1
+{
+    partial class ttNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txSDT = new System.Windows.Forms.TextBox();
+            this.txEmail = new System.Windows.Forms.TextBox();
+            this.txLuong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txMaNhanVien = new System.Windows.Forms.TextBox();
+            this.txHoTen = new System.Windows.Forms.TextBox();
+            this.txDiachi = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cbTo = new System.Windows.Forms.ComboBox();
+            this.cbChucVu = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Beige;
+            this.panel1.Controls.Add(this.cbChucVu);
+            this.panel1.Controls.Add(this.cbTo);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.txLuong);
+            this.panel1.Controls.Add(this.txEmail);
+            this.panel1.Controls.Add(this.txSDT);
+            this.panel1.Controls.Add(this.txDiachi);
+            this.panel1.Controls.Add(this.txHoTen);
+            this.panel1.Controls.Add(this.txMaNhanVien);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(854, 413);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số điện thoại";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(493, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Chức vụ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(493, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(47, 328);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Lương";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 259);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Tổ";
+            // 
+            // txSDT
+            // 
+            this.txSDT.Location = new System.Drawing.Point(47, 207);
+            this.txSDT.Name = "txSDT";
+            this.txSDT.Size = new System.Drawing.Size(198, 26);
+            this.txSDT.TabIndex = 12;
+            // 
+            // txEmail
+            // 
+            this.txEmail.Location = new System.Drawing.Point(493, 207);
+            this.txEmail.Name = "txEmail";
+            this.txEmail.Size = new System.Drawing.Size(353, 26);
+            this.txEmail.TabIndex = 13;
+            // 
+            // txLuong
+            // 
+            this.txLuong.Location = new System.Drawing.Point(47, 351);
+            this.txLuong.Name = "txLuong";
+            this.txLuong.Size = new System.Drawing.Size(170, 26);
+            this.txLuong.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Nhân Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(493, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hẹ và Tên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ngày Sinh";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(493, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Địa chỉ";
+            // 
+            // txMaNhanVien
+            // 
+            this.txMaNhanVien.Location = new System.Drawing.Point(47, 66);
+            this.txMaNhanVien.Name = "txMaNhanVien";
+            this.txMaNhanVien.Size = new System.Drawing.Size(200, 26);
+            this.txMaNhanVien.TabIndex = 9;
+            // 
+            // txHoTen
+            // 
+            this.txHoTen.Location = new System.Drawing.Point(493, 66);
+            this.txHoTen.Name = "txHoTen";
+            this.txHoTen.Size = new System.Drawing.Size(353, 26);
+            this.txHoTen.TabIndex = 10;
+            // 
+            // txDiachi
+            // 
+            this.txDiachi.Location = new System.Drawing.Point(493, 136);
+            this.txDiachi.Name = "txDiachi";
+            this.txDiachi.Size = new System.Drawing.Size(353, 26);
+            this.txDiachi.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(47, 136);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // cbTo
+            // 
+            this.cbTo.FormattingEnabled = true;
+            this.cbTo.Location = new System.Drawing.Point(47, 282);
+            this.cbTo.Name = "cbTo";
+            this.cbTo.Size = new System.Drawing.Size(198, 28);
+            this.cbTo.TabIndex = 16;
+            // 
+            // cbChucVu
+            // 
+            this.cbChucVu.FormattingEnabled = true;
+            this.cbChucVu.Location = new System.Drawing.Point(493, 282);
+            this.cbChucVu.Name = "cbChucVu";
+            this.cbChucVu.Size = new System.Drawing.Size(142, 28);
+            this.cbChucVu.TabIndex = 17;
+            // 
+            // ttNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 438);
+            this.Controls.Add(this.panel1);
+            this.Name = "ttNhanVien";
+            this.Text = "ttNhanVien";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txLuong;
+        private System.Windows.Forms.TextBox txEmail;
+        private System.Windows.Forms.TextBox txSDT;
+        private System.Windows.Forms.TextBox txDiachi;
+        private System.Windows.Forms.TextBox txHoTen;
+        private System.Windows.Forms.TextBox txMaNhanVien;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbChucVu;
+        private System.Windows.Forms.ComboBox cbTo;
+    }
+}
