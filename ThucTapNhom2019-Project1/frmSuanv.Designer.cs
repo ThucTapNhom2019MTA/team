1de9334cf21bd8a4c929a8d7b256aa5046b20cf1
Tran Dat <tranmanhdathsgs@gmail.com>
2019-08-05 22:28:35 +0700

    update

--- ThucTapNhom2019-Project1/frmSuanv.Designer.cs
+++ ThucTapNhom2019-Project1/frmSuanv.Designer.cs
@@ -52,22 +52,24 @@
             // 
             this.txbManv.Location = new System.Drawing.Point(60, 60);
             this.txbManv.Name = "txbManv";
+            this.txbManv.ReadOnly = true;
             this.txbManv.Size = new System.Drawing.Size(222, 22);
             this.txbManv.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(57, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.Size = new System.Drawing.Size(98, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Mã nhân viên";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(474, 35);
+            this.label2.Location = new System.Drawing.Point(474, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 17);
             this.label2.TabIndex = 2;
@@ -199,7 +201,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 488);
             this.Controls.Add(this.cbbChucvu);
             this.Controls.Add(this.cbbTo);
             this.Controls.Add(this.txbLuong);
