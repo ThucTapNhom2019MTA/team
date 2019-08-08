b7f902d9f4fd1943ac7e17ddca625a4e6e10b1e5
Tran Dat <tranmanhdathsgs@gmail.com>
2019-08-08 09:33:30 +0700

    update class NhanVien

--- ThucTapNhom2019-Project1/NhanVien.cs
+++ ThucTapNhom2019-Project1/NhanVien.cs
@@ -12,11 +12,33 @@ namespace ThucTapNhom2019_Project1
         private string hoten;
         private DateTime ngaysinh;
         private string diachi;
-        public NhanVien(string _manv,string _hoten,DateTime _ngaysinh,
-                        string _diachi,string _sdt,string _email,
-                        string _to,string _chucvu,int _luong)
-        {
+        private string sdt;
+        private string email;
+        private string to;
+        private string chucvu;
+        int luong;
 
+        public NhanVien(string manv, string hoten, DateTime ngaysinh, 
+                        string diachi, string sdt, string email, 
+                        string to, string chucvu)
+        {
+            this.manv = manv;
+            this.hoten = hoten;
+            this.ngaysinh = ngaysinh;
+            this.diachi = diachi;
+            this.sdt = sdt;
+            this.email = email;
+            this.to = to;
+            this.chucvu = chucvu;
         }
+
+        public string Manv { get => manv; set => manv = value; }
+        public string Hoten { get => hoten; set => hoten = value; }
+        public DateTime Ngaysinh { get => ngaysinh; set => ngaysinh = value; }
+        public string Diachi { get => diachi; set => diachi = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
+        public string Email { get => email; set => email = value; }
+        public string To { get => to; set => to = value; }
+        public string Chucvu { get => chucvu; set => chucvu = value; }
     }
 }
