118bd69bc110c131ca7392503d2965bdad1c282c
phamvandan <phamvandan@LAPTOP-2GH99NQG>
2019-08-15 09:30:13 +0700

    binding chuc vu with database

--- ThucTapNhom2019-Project1/frmSuanv.cs
+++ ThucTapNhom2019-Project1/frmSuanv.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace ThucTapNhom2019_Project1
         public frmSuanv(string maNv,int index,DataGridView datagv)
         {
             InitializeComponent();
+            bindingChucVuTxtBox();
             txbManv.Text = maNv;
             txbHoten.Text = datagv.Rows[index].Cells[1].Value.ToString();
             dateTimePicker.Value = Convert.ToDateTime(datagv.Rows[index].Cells[2].Value.ToString());
@@ -23,6 +25,20 @@ namespace ThucTapNhom2019_Project1
             txbEmail.Text = datagv.Rows[index].Cells[5].Value.ToString();
             txbLuong.Text = datagv.Rows[index].Cells[6].Value.ToString();
             cbbChucvu.Text = datagv.Rows[index].Cells[7].Value.ToString();
+        }
+
+        public void bindingChucVuTxtBox()
+        {
+            SqlConnection conn = ConnectSQLServer.getConnection();
+            conn.Open();
+            string strQueryDanhSach = "SELECT TenChucVu FROM dbo.ChucVu";
+            SqlDataAdapter da = new SqlDataAdapter(strQueryDanhSach, conn);
+            DataTable dtDanhSach = new DataTable();
+            da.Fill(dtDanhSach);
+            cbbChucvu.DataSource = dtDanhSach;
+            cbbChucvu.DisplayMember = "TenChucVu";
+            cbbChucvu.ValueMember = "TenChucVu";
+            conn.Close();
         }
     }
 }
