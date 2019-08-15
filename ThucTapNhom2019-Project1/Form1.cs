021567189af72ac1da9b2079a58d45cca18df93f
LoveHQH <huy441998@gmail.com>
2019-08-16 03:11:28 +0700

    Update Delete. Ae Hoàn thiện bổ sung thêm phần Add.

--- ThucTapNhom2019-Project1/Form1.cs
+++ ThucTapNhom2019-Project1/Form1.cs
@@ -16,6 +16,8 @@ namespace ThucTapNhom2019_Project1
         SqlConnection conn = ConnectSQLServer.getConnection();
         int soNhanVien;
         DataTable dtDanhSach;
+        string MaNVxoa;
+        string tenNV;
         public Form1()
         {
             InitializeComponent();
@@ -49,6 +51,8 @@ namespace ThucTapNhom2019_Project1
             conn.Open();
             if (e.RowIndex <6 && e.RowIndex >=0) {
                 string manv = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                tenNV = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                MaNVxoa = manv;
                 string strQuery = "SELECT a.TenTo, b.TenPhong FROM (SELECT TenTo,MaNhanVien FROM [dbo].[To], dbo.NhanVien WHERE NhanVien.MaTo = [To].MaTo) AS a," +
             "(SELECT TenPhong, MaNhanVien FROM[To], Phong, dbo.NhanVien WHERE Phong.MaPhong = [To].MaPhong AND dbo.NhanVien.MaTo = [To].MaTo) AS b WHERE a.MaNhanVien = b.MaNhanVien and a.MaNhanVien = "+"'"+manv+"'";
                 SqlDataAdapter da1 = new SqlDataAdapter(strQuery, conn);
@@ -118,6 +122,33 @@ namespace ThucTapNhom2019_Project1
                 conn.Close();
             }
 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn có chắc chắn xóa Nhân Viên : " + tenNV, "Xóa nhân viên", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes) {
+                conn.Open();
+                string query = "DELETE FROM [dbo].[NhanVien] WHERE MaNhanVien = " + "'" + MaNVxoa + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+                sda.SelectCommand.ExecuteNonQuery();
+
+                string strQueryDanhSach = "Select MaNhanVien as Mã, HoTen as [Họ và tên], " +
+                    "NgaySinh as [Ngày Sinh], DiaChi as [Địa Chỉ], SoDienThoai as [Số Điện Thoại], Email, " +
+                    "Luong as [Lương], TenChucVu AS [Tên chức vụ] from NhanVien, dbo.ChucVu " +
+                    "WHERE ChucVu.MaChucVu = NhanVien.MaChucVu";
+                SqlDataAdapter da = new SqlDataAdapter(strQueryDanhSach, conn);
+                dtDanhSach = new DataTable();
+                da.Fill(dtDanhSach);
+                dataGridView1.DataSource = dtDanhSach;
+                soNhanVien = dtDanhSach.Rows.Count;
+                conn.Close();
+            }
         }
 
     }
