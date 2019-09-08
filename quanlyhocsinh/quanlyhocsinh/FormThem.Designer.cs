a6c6080b277432601327cfcf2a4f51583e5e2171
capthang <capmanhthang@gmail.com>
2019-09-08 22:52:18 +0700

    thêm học sinh

---
+++ quanlyhocsinh/quanlyhocsinh/FormThem.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace quanlyhocsinh
+{
+    partial class FormThem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.txb_Sodt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txb_Diachi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txb_Hoten = new System.Windows.Forms.TextBox();
+            this.txb_Manv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(388, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 53);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Thêm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txb_Sodt
+            // 
+            this.txb_Sodt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Sodt.Location = new System.Drawing.Point(123, 279);
+            this.txb_Sodt.Name = "txb_Sodt";
+            this.txb_Sodt.Size = new System.Drawing.Size(294, 30);
+            this.txb_Sodt.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(119, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 22);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Số đt";
+            // 
+            // txb_Diachi
+            // 
+            this.txb_Diachi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Diachi.Location = new System.Drawing.Point(123, 176);
+            this.txb_Diachi.Name = "txb_Diachi";
+            this.txb_Diachi.Size = new System.Drawing.Size(298, 30);
+            this.txb_Diachi.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(119, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 22);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Giới tính";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(526, 176);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(294, 30);
+            this.dateTimePicker1.TabIndex = 23;
+            this.dateTimePicker1.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // txb_Hoten
+            // 
+            this.txb_Hoten.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Hoten.Location = new System.Drawing.Point(526, 87);
+            this.txb_Hoten.Name = "txb_Hoten";
+            this.txb_Hoten.Size = new System.Drawing.Size(298, 30);
+            this.txb_Hoten.TabIndex = 22;
+            // 
+            // txb_Manv
+            // 
+            this.txb_Manv.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Manv.Location = new System.Drawing.Point(123, 87);
+            this.txb_Manv.Name = "txb_Manv";
+            this.txb_Manv.Size = new System.Drawing.Size(294, 30);
+            this.txb_Manv.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(522, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 22);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Ngày sinh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(522, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 22);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Họ tên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 22);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Mã học sinh";
+            // 
+            // FormThem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 522);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txb_Sodt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txb_Diachi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txb_Hoten);
+            this.Controls.Add(this.txb_Manv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormThem";
+            this.Text = "FormThem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txb_Sodt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txb_Diachi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txb_Hoten;
+        private System.Windows.Forms.TextBox txb_Manv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- quanlyhocsinh/quanlyhocsinh/constringsql.cs
+++ quanlyhocsinh/quanlyhocsinh/constringsql.cs
@@ -11,8 +11,8 @@ namespace quanlyhocsinh
     {
         public static SqlConnection getConnection()
         {
-            //string connString = @"Data Source=DESKTOP-BBQPTV7\SQLEXPRESS;Initial Catalog=QuanLyHocSinh;Integrated Security=True";
-            string connString = @"Data Source=.; Initial Catalog=QuanLyHocSinh;Integrated Security=True";
+            string connString = @"Data Source=DESKTOP-BBQPTV7\SQLEXPRESS;Initial Catalog=QuanLyHocSinh;Integrated Security=True";
+            //string connString = @"Data Source=.; Initial Catalog=QuanLyHocSinh;Integrated Security=True";
             SqlConnection conn = new SqlConnection(connString);
             return conn;
         }
