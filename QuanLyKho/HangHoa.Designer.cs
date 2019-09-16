bd83fda3b8de14b1aa698a23aab946699e0ea9b6
thienquang98 <thienquang02468@gmail.com>
2019-09-16 21:43:14 +0700

    form hang hoa - quan ly kho

--- QuanLyKho/HangHoa.Designer.cs
+++ QuanLyKho/HangHoa.Designer.cs
@@ -29,38 +29,328 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fieldTim = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.btnhuy = new System.Windows.Forms.Button();
+            this.btluu = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.btsua = new System.Windows.Forms.Button();
+            this.btthem = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fieldSL = new System.Windows.Forms.MaskedTextBox();
+            this.fieldNgayNhap = new System.Windows.Forms.DateTimePicker();
+            this.fieldDonViTinh = new System.Windows.Forms.TextBox();
+            this.fieldTenHH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fieldMaHH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(821, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // comboBox1
+            // panel3
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Theo Tên",
-            "Theo Giá"});
-            this.comboBox1.Location = new System.Drawing.Point(26, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.fieldTim);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(-21, 211);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(836, 194);
+            this.panel3.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(832, 191);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.DataPropertyName = "mahh";
+            this.Column1.HeaderText = "Mã HH";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.DataPropertyName = "tenhh";
+            this.Column2.HeaderText = "Tên HH";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.DataPropertyName = "soluong";
+            this.Column3.HeaderText = "Số Lượng";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.DataPropertyName = "donvitinh";
+            this.Column4.HeaderText = "Đơn Vị Tính";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.DataPropertyName = "ngaynhap";
+            this.Column5.HeaderText = "Ngày Nhập";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // fieldTim
+            // 
+            this.fieldTim.Location = new System.Drawing.Point(194, 37);
+            this.fieldTim.Name = "fieldTim";
+            this.fieldTim.Size = new System.Drawing.Size(162, 20);
+            this.fieldTim.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(126, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Tên HH";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tìm kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnthoat);
+            this.panel2.Controls.Add(this.btnhuy);
+            this.panel2.Controls.Add(this.btluu);
+            this.panel2.Controls.Add(this.btxoa);
+            this.panel2.Controls.Add(this.btsua);
+            this.panel2.Controls.Add(this.btthem);
+            this.panel2.Location = new System.Drawing.Point(-20, 154);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(835, 51);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(754, 16);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.TabIndex = 3;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            // 
+            // btnhuy
+            // 
+            this.btnhuy.Location = new System.Drawing.Point(629, 16);
+            this.btnhuy.Name = "btnhuy";
+            this.btnhuy.Size = new System.Drawing.Size(75, 23);
+            this.btnhuy.TabIndex = 3;
+            this.btnhuy.Text = "Hủy";
+            this.btnhuy.UseVisualStyleBackColor = true;
+            // 
+            // btluu
+            // 
+            this.btluu.Location = new System.Drawing.Point(489, 16);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(75, 23);
+            this.btluu.TabIndex = 3;
+            this.btluu.Text = "Lưu";
+            this.btluu.UseVisualStyleBackColor = true;
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(330, 16);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(75, 23);
+            this.btxoa.TabIndex = 2;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.UseVisualStyleBackColor = true;
+            // 
+            // btsua
+            // 
+            this.btsua.Location = new System.Drawing.Point(169, 16);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(75, 23);
+            this.btsua.TabIndex = 1;
+            this.btsua.Text = "Sửa";
+            this.btsua.UseVisualStyleBackColor = true;
+            this.btsua.Click += new System.EventHandler(this.btsua_Click);
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(26, 16);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(75, 23);
+            this.btthem.TabIndex = 0;
+            this.btthem.Text = "Thêm";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fieldSL);
+            this.panel1.Controls.Add(this.fieldNgayNhap);
+            this.panel1.Controls.Add(this.fieldDonViTinh);
+            this.panel1.Controls.Add(this.fieldTenHH);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.fieldMaHH);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-21, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(836, 134);
+            this.panel1.TabIndex = 3;
+            // 
+            // fieldSL
+            // 
+            this.fieldSL.Location = new System.Drawing.Point(365, 17);
+            this.fieldSL.Mask = "00000000000000000000000000000000";
+            this.fieldSL.Name = "fieldSL";
+            this.fieldSL.Size = new System.Drawing.Size(171, 20);
+            this.fieldSL.TabIndex = 10;
+            // 
+            // fieldNgayNhap
+            // 
+            this.fieldNgayNhap.Location = new System.Drawing.Point(639, 17);
+            this.fieldNgayNhap.Name = "fieldNgayNhap";
+            this.fieldNgayNhap.Size = new System.Drawing.Size(191, 20);
+            this.fieldNgayNhap.TabIndex = 9;
+            // 
+            // fieldDonViTinh
+            // 
+            this.fieldDonViTinh.Location = new System.Drawing.Point(365, 70);
+            this.fieldDonViTinh.Name = "fieldDonViTinh";
+            this.fieldDonViTinh.Size = new System.Drawing.Size(171, 20);
+            this.fieldDonViTinh.TabIndex = 6;
+            // 
+            // fieldTenHH
+            // 
+            this.fieldTenHH.Location = new System.Drawing.Point(92, 70);
+            this.fieldTenHH.Name = "fieldTenHH";
+            this.fieldTenHH.Size = new System.Drawing.Size(162, 20);
+            this.fieldTenHH.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(572, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Ngày Nhập";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Đơn Vị Tính";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Số Lượng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên HH";
+            // 
+            // fieldMaHH
+            // 
+            this.fieldMaHH.Location = new System.Drawing.Point(92, 20);
+            this.fieldMaHH.Name = "fieldMaHH";
+            this.fieldMaHH.ReadOnly = true;
+            this.fieldMaHH.Size = new System.Drawing.Size(162, 20);
+            this.fieldMaHH.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã HH :";
             // 
             // HangHoa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(821, 406);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "HangHoa";
             this.Text = "KhoHangHoa";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +359,33 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.TextBox fieldTim;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.Button btnhuy;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox fieldSL;
+        private System.Windows.Forms.DateTimePicker fieldNgayNhap;
+        private System.Windows.Forms.TextBox fieldDonViTinh;
+        private System.Windows.Forms.TextBox fieldTenHH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fieldMaHH;
+        private System.Windows.Forms.Label label1;
     }
 }
--- QuanLyKho/HangHoa.cs
+++ QuanLyKho/HangHoa.cs
@@ -22,5 +22,68 @@ namespace QuanLyKho
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = @"SELECT * FROM DMHangHoa";
+            DataTable data = Database.getTable(sql);  //lấy dữ liệu từ bảng KhachHang
+            if (fieldTim.Text != "")
+            {
+                data.DefaultView.RowFilter = "tenhh LIKE '%" + fieldTim.Text + "%'";  //filter lọc dữ liệu
+            }
+            else
+            {
+                data.DefaultView.RowFilter = "";
+            }
+
+            dataGridView1.DataSource = data;  //gán giá trị vào datagridview
+        }
+
+        private void btsua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void motxt()
+        {
+            fieldDonViTinh.Enabled = true;
+            fieldNgayNhap.Enabled = true;
+            fieldSL.Enabled = true;
+            fieldTenHH.Enabled = true;
+        }
+
+        private void dongtxt()
+        {
+            fieldDonViTinh.Enabled = false;
+            fieldNgayNhap.Enabled = false;
+            fieldSL.Enabled = false;
+            fieldTenHH.Enabled = false;
+        }
+
+        private void mobtn()
+        {
+            btthem.Enabled = true;
+            btsua.Enabled = true;
+            btxoa.Enabled = true;
+            btnhuy.Enabled = false;
+            btluu.Enabled = false;
+        }
+
+        private void dongbtn()
+        {
+            btthem.Enabled = false;
+            btsua.Enabled = false;
+            btxoa.Enabled = false;
+            btnhuy.Enabled = true;
+            btluu.Enabled = true;
+        }
+
+        string state = "";
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            motxt();
+            dongbtn();
+            state = "insert";
+        }
     }
 }
