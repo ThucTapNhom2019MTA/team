79ebcae24dc7376cff77853e0c7af20656e027a5
thienquang98 <thienquang02468@gmail.com>
2019-09-22 10:47:32 +0700

    them tai khoan - form quan ly tai khoan - quan ly kho

--- QuanLyKho/QuanLyTaiKhoan.Designer.cs
+++ QuanLyKho/QuanLyTaiKhoan.Designer.cs
@@ -82,6 +82,7 @@
             this.button1.TabIndex = 15;
             this.button1.Text = "Thêm";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // fieldMatKhau
             // 
--- QuanLyKho/QuanLyTaiKhoan.cs
+++ QuanLyKho/QuanLyTaiKhoan.cs
@@ -16,5 +16,63 @@ namespace QuanLyKho
         {
             InitializeComponent();
         }
+
+
+        private bool validate()
+        {
+            if (fieldTenDN.Text == "" || fieldMatKhau.Text == "")
+            {
+                MessageBox.Show("Bạn phải điền đầy đủ các trường !");
+                return false;
+            }
+            return true;
+        }
+
+        private void hien()
+        {
+            string sql = @"SELECT * FROM TaiKhoanNV where Role <> 'A'";
+
+            dataGridView1.DataSource = Database.getTable(sql);
+
+            binding();
+        }
+
+        private void binding()
+        {
+            fieldMaTK.DataBindings.Clear();
+            fieldTenDN.DataBindings.Clear();
+            fieldMatKhau.DataBindings.Clear();
+
+            fieldMaTK.DataBindings.Add("Text", dataGridView1.DataSource, "MaTK");
+            fieldTenDN.DataBindings.Add("Text", dataGridView1.DataSource, "TenTK");
+            fieldMatKhau.DataBindings.Add("Text", dataGridView1.DataSource, "MKTK");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (validate())
+            {
+                string sql = @"INSERT INTO [QLKhoHang].[dbo].[TaiKhoanNV]
+                                   ([TenTK]
+                                   ,[Role]
+                                   ,[MKTK])
+                             VALUES
+                                   ('" + fieldTenDN.Text + @"'
+                                   ,'N'
+                                   ,'" + fieldMatKhau.Text + @"'
+                                    )";
+
+                if (Database.Query(sql) == -1)
+                {
+                    MessageBox.Show("Có lỗi trong quá trình thêm !");
+                }
+                else
+                {
+                    MessageBox.Show("Thêm thành công !");
+                    hien();
+                }
+            }
+        }
     }
 }
