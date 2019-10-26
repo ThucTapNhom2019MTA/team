33d9b06c988470a55eea2fd6b21c169cf3fb8606
Tran Dat <tranmanhdathsgs@gmail.com>
2019-10-26 22:50:13 +0700

    update

--- ThucTapNhom2019-Project1/ConnectSQLServer.cs
+++ ThucTapNhom2019-Project1/ConnectSQLServer.cs
@@ -15,8 +15,8 @@ namespace ThucTapNhom2019_Project1
             //string connString = @"Data Source=LAPTOP-2GH99NQG\PHAMDANSQL;Initial Catalog=QuanLiNhanSu;Integrated Security=True";
             //string connString = "Data Source=.; Initial Catalog = QuanLiNhanSu; Integrated Security = True";
             //string connString = @"Data Source=.\SQLSERVER;Initial Catalog=QuanLiNhanSu;Integrated Security=True";
-            string connString = @"Data Source=DESKTOP-BBQPTV7\SQLEXPRESS;Initial Catalog=QuanLiNhanSu;Integrated Security=True";
-            //string connString = @"Data Source=DESKTOP-U1I0DJ3\SQLSERVER;Initial Catalog=QuanLiNhanSu;Integrated Security=True";
+            //string connString = @"Data Source=DESKTOP-BBQPTV7\SQLEXPRESS;Initial Catalog=QuanLiNhanSu;Integrated Security=True";
+            string connString = @"Data Source=DESKTOP-U1I0DJ3\SQLSERVER;Initial Catalog=QuanLiNhanSu;Integrated Security=True";
             SqlConnection conn = new SqlConnection(connString);
             return conn;
         }
