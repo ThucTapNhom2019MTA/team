f329d7d52b1c088b202bca5322e4aaf0e10c614d
thienquang98 <thienquang02468@gmail.com>
2019-10-28 13:51:39 +0700

    xuat hang - kho

--- QuanLyKho/XuatHang.cs
+++ QuanLyKho/XuatHang.cs
@@ -117,5 +117,154 @@ namespace QuanLyKho
             dongtxt();
             hien();
         }
+
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            motxt();
+            dongbtn();
+            state = "insert";
+        }
+
+        private void btsua_Click(object sender, EventArgs e)
+        {
+            motxt();
+            dongbtn();
+            state = "update";
+            binding();
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            motxt();
+            dongbtn();
+            state = "delete";
+            binding();
+        }
+
+        private void btluu_Click(object sender, EventArgs e)
+        {
+            if (validate())
+            {
+
+
+
+                if (state == "insert")
+                {
+                    //MessageBox.Show("INSERT INTO PhieuNhap(MaPhieuNhap, TenNCC, TenMH, SoLuongNhap,GiaNhap,TienDaThanhToan,NgayThanhToan) VALUES (" + Convert.ToInt32(txtMaPhieuNhap.Text) + ",N'" + txtTenNCC.Text + "',N'" + txtTenMH.Text + "'," + Convert.ToInt32(txtSoLuongNhap.Text) + "," + Convert.ToInt32(txtGiaNhap.Text) + "," + Convert.ToInt32(txtTienDaThanhToan.Text) + ",'"+txtNgayThanhToan.Value.ToShortDateString().ToString() +"')");
+                    string sql = @"INSERT INTO [QLKhoHang].[dbo].[PhieuXuat]
+           ([NgayXuat]
+           ,[NguoiXuat]
+           ,[MaKH]
+           ,[SoLuongXuat]
+           ,[MaHH])
+     VALUES
+           ('" + fieldNgayXuat.Value.ToString("yyyy/MM/dd") + @"'
+           ,N'" + fieldNguoiXuat.Text + @"'
+           ," + fieldMaKH.SelectedValue + @"
+           ," + Convert.ToInt64(fieldSoLuongXuat.Text) + @"
+           ," + fieldMaHH.SelectedValue + @"
+)";
+
+                    if (Database.Query(sql) != -1)
+                    {
+                        MessageBox.Show("Thêm thành công !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Có lỗi trong quá trình thêm");
+                    }
+                }
+                //end add
+                else if (state == "update")
+                {
+                    string sql = @"UPDATE [QLKhoHang].[dbo].[PhieuXuat]
+                       SET [NgayXuat] ='" + fieldNgayXuat.Value.ToString("yyyy/MM/dd") + @"'
+                          ,[NguoiXuat] = N'" + fieldNguoiXuat.Text + @"'
+                          ,[MaKH] =" + fieldMaKH.SelectedValue + @"
+                          ,[SoLuongXuat] =" + Convert.ToInt64(fieldSoLuongXuat.Text) + @"
+                          ,[MaHH] = " + fieldMaHH.SelectedValue + @"
+                     WHERE MaPXK = " + Convert.ToInt64(fieldMaPXK.Text);
+
+
+
+                    if (Database.Query(sql) != -1)
+                    {
+                        MessageBox.Show("Sửa thành công !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Có lỗi trong quá trình Sửa");
+                    }
+
+                }
+
+                hien();
+                dongtxt();
+                mobtn();
+            }//end validate
+            if (state == "delete" && validate())
+            {
+                string sql = @"DELETE [QLKhoHang].[dbo].[PhieuXuat]
+                WHERE MaPXK = " + Convert.ToInt64(fieldMaPXK.Text);
+
+
+                if (Database.Query(sql) != -1)
+                {
+                    MessageBox.Show("Xóa thành công !");
+                }
+                else
+                {
+                    MessageBox.Show("Có lỗi trong quá trình Xóa");
+                }
+
+                hien();
+                dongtxt();
+                mobtn();
+            }
+        }
+
+        private void btnhuy_Click(object sender, EventArgs e)
+        {
+            mobtn();
+            dongtxt();
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            new Form1().Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Database.ExportExcel(dataGridView1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = @"SELECT     PhieuXuat.NgayXuat, PhieuXuat.NguoiXuat, PhieuXuat.MaKH, PhieuXuat.SoLuongXuat, PhieuXuat.MaHH, PhieuXuat.MaPXK, DMKhachHang.tenkh, 
+                      DMHangHoa.tenhh
+                      FROM         DMHangHoa INNER JOIN
+                      PhieuXuat ON DMHangHoa.mahh = PhieuXuat.MaHH INNER JOIN
+                      DMKhachHang ON PhieuXuat.MaKH = DMKhachHang.makh";
+            DataTable data = Database.getTable(sql);  //lấy dữ liệu từ bảng KhachHang
+            if (txtTim.Text != "")
+            {
+                data.DefaultView.RowFilter = "NguoiXuat LIKE '%" + txtTim.Text + "%' OR tenkh LIKE '%" + txtTim.Text + "%' OR tenhh LIKE '%" + txtTim.Text + "%'";  //filter lọc dữ liệu
+            }
+            else
+            {
+                data.DefaultView.RowFilter = "";
+            }
+
+            dataGridView1.DataSource = data;  //gán giá trị vào datagridview
+        }
+
+
+
+
+
+
+
     }
 }
