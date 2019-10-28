e65c9d4fab8caf924acdc3bb84ed07dc43b99acb
thienquang98 <thienquang02468@gmail.com>
2019-10-28 12:03:24 +0700

    Xuat hang - Kho

--- QuanLyKho/XuatHang.cs
+++ QuanLyKho/XuatHang.cs
@@ -16,5 +16,81 @@ namespace QuanLyKho
         {
             InitializeComponent();
         }
+
+        private bool validate()
+        {
+            if (fieldMaHH.Text == "" || fieldMaKH.Text == "" || fieldNguoiXuat.Text == "" || fieldSoLuongXuat.Text == "")
+            {
+                MessageBox.Show("Bạn phải điền đầy đủ các trường !");
+                return false;
+            }
+            return true;
+        }
+
+        private void hien()
+        {
+            string sql = @"SELECT     PhieuXuat.NgayXuat, PhieuXuat.NguoiXuat, PhieuXuat.MaKH, PhieuXuat.SoLuongXuat, PhieuXuat.MaHH, PhieuXuat.MaPXK, DMKhachHang.tenkh, 
+                      DMHangHoa.tenhh
+                FROM  DMHangHoa INNER JOIN
+                      PhieuXuat ON DMHangHoa.mahh = PhieuXuat.MaHH INNER JOIN
+                      DMKhachHang ON PhieuXuat.MaKH = DMKhachHang.makh";
+
+            dataGridView1.DataSource = DbHelper.getTable(sql);
+        }
+
+        private void binding()
+        {
+            fieldMaHH.DataBindings.Clear();
+            fieldMaKH.DataBindings.Clear();
+            fieldMaPXK.DataBindings.Clear();
+            fieldNgayXuat.DataBindings.Clear();
+            fieldNguoiXuat.DataBindings.Clear();
+            fieldSoLuongXuat.DataBindings.Clear();
+
+            fieldMaHH.DataBindings.Add("Text", dataGridView1.DataSource, "MaHH");
+            fieldMaKH.DataBindings.Add("Text", dataGridView1.DataSource, "MaKH");
+            fieldMaPXK.DataBindings.Add("Text", dataGridView1.DataSource, "MaPXK");
+            fieldNgayXuat.DataBindings.Add("Text", dataGridView1.DataSource, "NgayXuat");
+            fieldNguoiXuat.DataBindings.Add("Text", dataGridView1.DataSource, "NguoiXuat");
+            fieldSoLuongXuat.DataBindings.Add("Text", dataGridView1.DataSource, "SoLuongXuat");
+
+        }
+        private void motxt()
+        {
+            fieldMaHH.Enabled = true;
+            fieldMaKH.Enabled = true;
+            fieldNgayXuat.Enabled = true;
+            fieldNguoiXuat.Enabled = true;
+            fieldSoLuongXuat.Enabled = true;
+        }
+
+        private void dongtxt()
+        {
+            fieldMaHH.Enabled = false;
+            fieldMaKH.Enabled = false;
+            fieldNgayXuat.Enabled = false;
+            fieldNguoiXuat.Enabled = false;
+            fieldSoLuongXuat.Enabled = false;
+        }
+
+        private void mobtn()
+        {
+            btluu.Enabled = false;
+            btnhuy.Enabled = false;
+            btthem.Enabled = true;
+            btsua.Enabled = true;
+            btxoa.Enabled = true;
+        }
+
+        private void dongbtn()
+        {
+            btluu.Enabled = true;
+            btnhuy.Enabled = true;
+            btthem.Enabled = false;
+            btsua.Enabled = false;
+            btxoa.Enabled = false;
+        }
+
+        string state = "";
     }
 }
